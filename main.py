f0d06d2a1b90fe8d8e87c37340315b9478844d28
arti163 <40715281+arti163@users.noreply.github.com>
2025-05-10 17:31:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 app = FastAPI()
 
 @app.get("/")
 async def read_item(param: str):
 	return {"Otrzymany Parametr:", param}
+
+client = TestClient(app)
+
+def test_read_item_with_param():
+    response = client.get("/?param=testowa_wartosc")
+    assert response.status_code == 200
+    assert response.json() == {"otrzymany_parametr": "testowa_wartosc"}
+
+def test_read_item_without_param():
+    response = client.get("/")
+    assert response.status_code == 422 # FastAPI automatycznie waliduje brak wymaganego parametru
